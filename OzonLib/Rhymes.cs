53a791e14b21711bda125da689b42255399ff7e7
Svyatoslav <slavaanufriev@gmail.com>
2023-08-10 14:02:58 +0300

    Add Rhymes and tests

---
+++ OzonLib/Rhymes.cs
@@ -0,0 +1,51 @@
+﻿namespace OzonLib;
+
+public class Rhymes
+{
+
+    public static void Run()
+    {
+        int dictQnt = int.Parse(Console.ReadLine());
+        string[] dict = new string[dictQnt];
+        for (int i = 0; i < dictQnt; i++)
+            dict[i] = Console.ReadLine();
+        
+        int requestsQnt = int.Parse(Console.ReadLine());
+        string[] requests = new string[requestsQnt];
+        for (int i = 0; i < requestsQnt; i++)
+            requests[i] = Console.ReadLine();
+
+        foreach (var request in requests)
+        {
+            Console.WriteLine(FindLongestSuffix(request, dict));
+        }
+    }
+
+    public static string FindLongestSuffix(string request, IEnumerable<string> dict)
+    {
+        string wordWithMaxSuffix = string.Empty;
+        int maxSuffixLength = 0;
+
+        foreach (string str in dict)
+        {
+            for (int j = 1; j <= str.Length; j++)
+            {
+                string substring = str.Substring(str.Length - j, j);
+                if (substring.Length > request.Length)
+                    break;
+
+                if (request.Substring(request.Length - j, j) == substring && substring.Length > maxSuffixLength)
+                {
+                    maxSuffixLength = substring.Length;
+                    if (str != request)
+                        wordWithMaxSuffix = str;
+                }
+            }
+        }
+
+        if (wordWithMaxSuffix == "")
+            return dict.ElementAt(new Random().Next(0, dict.Count() - 1));
+
+        return wordWithMaxSuffix;
+    }
+}
---
+++ OzonLibTests/RhymesTest.cs
@@ -0,0 +1,44 @@
+﻿using OzonLib;
+
+namespace OzonLibTests;
+
+public class RhymesTest
+{
+    private List<string> _dict = new List<string>{ "task", "decide", "id" };
+    [Fact]
+    public void Test1()
+    {
+        var result = Rhymes.FindLongestSuffix("flask", _dict);
+        Assert.Equal("task", result);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var result = Rhymes.FindLongestSuffix("code", _dict);
+        Assert.Equal("decide", result);
+    }
+
+    [Fact]
+    public void Test4()
+    {
+        var result = Rhymes.FindLongestSuffix("forces", _dict);
+        Assert.Equal("task", result);
+    }
+
+    [Fact]
+    public void Test5()
+    {
+        var result = Rhymes.FindLongestSuffix("id", _dict);
+        Assert.Equal("decide", result);
+    }
+
+    [Fact]
+    public void Test6()
+    {
+        var result = Rhymes.FindLongestSuffix("ask", _dict);
+        Assert.Equal("task", result);
+    }
+
+
+}
